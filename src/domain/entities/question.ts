dd8dfd4ad9540cce95def09f1cd2c08fd1d43e81
Rafael <rafael.sergio@nextpage.com.br>
2024-12-19 11:46:02 -0300

    add eslint config

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,7 @@
 import { Slug } from './value-objects/slug'
-import { Entity } from '../../core/entities/entity'
-import type { UniqueEntityId } from '../../core/entities/unique-entity-id'
-import type { Optional } from '../../core/types/optional'
+import { Entity } from '@/core/entities/entity'
+import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import type { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
 
 interface QuestionProps {
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, test } from 'vitest'
 import { AnswerQuestion } from './answer-question'
 import type { AnswersRepository } from '../repositories/answers-repository'
 
