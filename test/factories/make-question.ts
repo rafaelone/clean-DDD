b525c0a0a0847d536b1658882523e542be627405
Rafael <rafael.sergio@nextpage.com.br>
2024-12-20 06:38:03 -0300

    feat: delete question

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   type QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(ovveride: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    authorId: new UniqueEntityId(),
-    content: 'Example content',
-    ...ovveride,
-  })
+export function makeQuestion(
+  ovveride: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityId(),
+      content: faker.lorem.text(),
+      ...ovveride,
+    },
+    id,
+  )
 
   return question
 }
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -17,4 +17,24 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     return question
   }
+
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    if (itemIndex === -1) {
+      throw new Error('Question not found.')
+    }
+
+    this.items.splice(itemIndex, 1)
+  }
 }
